e12bc48ac192fd56ce6a3ce494f489230b856578
Chandani <nittianchandani@gmail.com>
2026-02-10 12:12:07 +0530

    Variable: using third variable

--- Addition.java
+++ Addition.java
@@ -2,6 +2,9 @@ public class Addition {
     public static void main(String a[]) {
         int num1 = 2;
         int num2 = 5;
-        System.out.println(num1+num2);
+        int result = num1 + num2;
+        // System.out.println(num1+num2);
+                System.out.println(result);
+
     }
 }
