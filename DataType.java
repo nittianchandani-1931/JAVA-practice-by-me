7bb164e44f2f900d3842bc4f1761ae3ae4277982
Chandani <nittianchandani@gmail.com>
2026-02-10 12:52:50 +0530

    Datatype

---
+++ DataType.java
@@ -0,0 +1,15 @@
+public class DataType {
+    public static void main(String a[]){
+        int num =2;
+        byte by = 124;
+        short sh = 554;
+        long l = 3339l;
+        float f = 3.5f;
+        double  d = 4.6;
+        char c = 'V';
+
+        boolean  b = true;
+
+
+    }
+}
