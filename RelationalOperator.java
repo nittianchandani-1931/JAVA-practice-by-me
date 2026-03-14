35b8d5f2fbc1904ac1529f75f9f8fc119c1da963
Chandani <nittianchandani@gmail.com>
2026-03-14 20:27:52 +0530

    Realtional operator

---
+++ RelationalOperator.java
@@ -0,0 +1,14 @@
+public class RelationalOperator {
+    public static void main(String[] args) {
+        int x = 6;
+        int y = 4;
+        // boolean result =x<y;
+        // boolean result1 =x>y;
+        // boolean result = x <= y;
+        // boolean result = x >= y;
+        // boolean result = x != y;
+        boolean result = x == y;
+
+        System.out.println(result);
+    }
+}
