87a2493fe55bebe859b9712334d9eb886d6ce991
Chandani <nittianchandani@gmail.com>
2026-03-16 01:22:14 +0530

    Do while loop implementation

---
+++ Dowhileloop.java
@@ -0,0 +1,12 @@
+public class Dowhileloop {
+    public static void main(String[] args)
+    {
+        int i = 5;
+        do{
+            System.out.println("Hii "+i);
+            i++;
+
+        } while(i<=4);
+
+    }
+}
