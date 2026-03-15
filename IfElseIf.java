b87bec7b554aa10269129cf288be26f9e95be39d
Chandani <nittianchandani@gmail.com>
2026-03-15 14:01:55 +0530

    use of If else if conditions

---
+++ IfElseIf.java
@@ -0,0 +1,16 @@
+public class IfElseIf {
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 6;
+        int z = 9;
+
+        if (x > y && x > z)
+
+            System.out.println(x);
+
+        else if (y > x )
+            System.out.println(y);
+        else
+            System.out.println(z);
+    }
+}
