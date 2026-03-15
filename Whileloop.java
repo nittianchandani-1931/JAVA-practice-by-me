55de3426e30ef5c69823b6d79e470d90c49267a5
Chandani <nittianchandani@gmail.com>
2026-03-15 21:28:22 +0530

    Creation of while loop and nested while loop

---
+++ Whileloop.java
@@ -0,0 +1,28 @@
+public class Whileloop {
+    public static void main(String[] args) {
+        // int i = 1;
+        // while (i<=5)
+
+        // {
+
+        //     System.out.println("Helloo " + i);
+        //     i++;
+        // }
+        // System.out.println("Byee "+ i);
+
+
+int i =1;
+while(i<=4){
+    System.out.println("hii  "+i);
+    int j =1;
+    while(j<=3){
+        System.out.println("Hello "+j);
+        j++;
+
+    }
+    i++;
+}
+System.out.println("Byee " + i);
+
+    }
+}
