0e120cd8138adc745ba83cef7de11831a466da0d
Chandani <nittianchandani@gmail.com>
2026-03-15 13:36:59 +0530

    Implementation of if else

---
+++ Ifelse.java
@@ -0,0 +1,17 @@
+public class Ifelse {
+    public static void main(String[] args) {
+        int x = 18;
+        int y = 5;
+        // if (x>10 && x<=20) //11-20
+        // System.out.println("Chandani");
+        // else
+        // System.out.println("Engineer");
+        if (x > y)
+          {  System.out.println(x);
+                        System.out.println("Welcome to my learning");   //for multiple statement in if we use curly braces not for single braces
+          }
+        else
+            System.out.println(y);
+
+    }
+}
