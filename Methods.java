b402f50ad5d198d515ce955146c2f6292d1f85ce
Chandani <nittianchandani@gmail.com>
2026-03-18 21:51:22 +0530

    Introduction of methods in java

---
+++ Methods.java
@@ -0,0 +1,22 @@
+class Computer {
+
+    public void playMusic() {
+        System.out.println("Music Playing....");
+    }
+
+    public String getMeAPen(int cost) {
+        if (cost >=10)
+        return "Pen";
+        else 
+        return "Nothing";
+    }
+}
+
+class Methods {
+    public static void main(String[] args) {
+        Computer obj = new Computer();
+        obj.playMusic();
+      String str =  obj.getMeAPen(15);
+      System.out.println(str);
+    }
+}
