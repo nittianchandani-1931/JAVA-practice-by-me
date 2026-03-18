1a66cd7d024e95d458fb376bec7dfd25efa8f041
Chandani <nittianchandani@gmail.com>
2026-03-19 01:18:15 +0530

    Implementation of method overloading

---
+++ MethodOverloading.java
@@ -0,0 +1,22 @@
+class Calculator {
+    public int add(int n1, int n2) {
+        return n1 + n2;
+    }
+
+    public int add(int n1, int n2, int n3) {
+        return n1 + n2 + n3;
+    }
+
+    public double add(double n1, int n2) {
+        return n1 + n2;
+    }
+
+}
+
+public class MethodOverloading {
+    public static void main(String a[]) {
+        Calculator obj = new Calculator();
+        double r1 = obj.add(3.89, 4);
+        System.out.println(r1);
+    }
+}
