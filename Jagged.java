2ce833d7eba5cf689b9578854bcde10f7882b34f
Chandani <nittianchandani@gmail.com>
2026-03-23 23:30:28 +0530

    Implementation of Jagged array and 3D array

---
+++ Jagged.java
@@ -0,0 +1,58 @@
+// public class Jagged {
+
+//     public static void main(String[] args) {
+//         int nums[][] = new int[3][]; // jagged
+
+//         nums[0] = new int[3];
+//         nums[1] = new int[4];
+//         nums[2] = new int[2];
+
+//         for (int i = 0; i < nums.length; i++) {
+//             for (int j = 0; j < nums[i].length; j++) {
+//                             for (int k = 0; k< nums[i].length; k++) {
+
+//                 nums[i][j] = (int) (Math.random() * 10);
+//             }
+
+//         }
+
+//         for (int[] n : nums) {
+//             for (int m : n) {
+//                 System.out.print(m+ " ");
+//             }
+//             System.out.println();
+//         }
+//         System.out.println("-----");
+//     }
+//         }
+//     }
+
+public class Jagged {
+
+    public static void main(String[] args) {
+        int nums[][][]= new int[3][4][5]; // 3D array
+
+       
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[i].length; j++) {
+                            for (int k = 0; k < nums[i][j].length; k++) {
+
+
+                nums[i][j][k] = (int) (Math.random() * 10);
+            }
+
+        }
+
+        for (int[][] n : nums) {
+            for (int[] m : n) {
+                for(int x : m){
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("-----");
+    }
+        }
+    }
+}
