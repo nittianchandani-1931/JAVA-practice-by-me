485c00b33205707bdd26967009857f8420634c00
Chandani <nittianchandani@gmail.com>
2026-04-01 19:47:34 +0530

    Introduction of Mutable and immutable string

---
+++ Typestring.java
@@ -0,0 +1,12 @@
+public class Typestring {
+    public static void main(String[] args) {
+        String name = "chandani";
+        name = name + " kumari";
+
+        System.out.println("Hello " + name);
+
+        String s1 = "Navin";
+        String s2 = "Navin";
+        System.out.println(s1 == s2);
+    }
+}
