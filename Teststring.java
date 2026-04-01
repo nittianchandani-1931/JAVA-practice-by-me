4e49b5e652394d96fb40c5309e31e9e65a3ce207
Chandani <nittianchandani@gmail.com>
2026-04-01 18:07:44 +0530

    Introduction to string and objects in it

---
+++ Teststring.java
@@ -0,0 +1,10 @@
+public class Teststring {
+    public static void main(String[] args){
+        // String name = new String("Chandani");
+String name = "chandani";
+        System.out.println("Hello  " + name);
+        // System.out.println(name.hashCode());
+        // System.out.println(name.charAt(1));
+        System.out.println(name.concat(" kumari"));
+    }
+}
