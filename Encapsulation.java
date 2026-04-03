0fce458340b3bc3b63c878d07b96d3ea80f8f31f
Chandani <nittianchandani@gmail.com>
2026-04-04 02:40:00 +0530

    Introduction and implementation of Encapsulation and methods

---
+++ Encapsulation.java
@@ -0,0 +1,26 @@
+class Human {
+    private int age ;
+    private String name ;
+
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int a){
+        age = a;
+    }
+    public String  getName(){
+        return name;
+    }
+    public void setName(String b){
+        name = b;
+    }
+}
+
+public class Encapsulation {
+    public static void main(String a[]) {
+        Human obj = new Human();
+        obj.setAge(23);
+        obj.setName ("Vikas");
+        System.out.println(obj.getName() + " - " + obj.getAge());
+    }
+}
