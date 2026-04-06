f6f80535e224b9161fcf3abee0910d44c63b2ba6
Chandani <nittianchandani@gmail.com>
2026-04-07 00:44:24 +0530

    Use of this keyword

--- This.java
+++ This.java
@@ -6,24 +6,24 @@ class Human {
         return age;
     }
 
-    public void setAge(int a) {
-        age = a;
+    public void setAge(int age) {
+        this.age = age;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String n) {
-        name = n;
+    public void setName(String name) {
+        this.name = name;
     }
 }
 
-public class This{
-public static void main(String a[]){
-    Human obj = new Human();
-    obj.setAge(30);
-    obj.setName("Vikas");
-    System.out.println(obj.getName() + ":" +obj.getAge());
-}
+public class This {
+    public static void main(String a[]) {
+        Human obj = new Human();
+        obj.setAge(30);
+        obj.setName("Vikas");
+        System.out.println(obj.getName() + ":" + obj.getAge());
+    }
 }
