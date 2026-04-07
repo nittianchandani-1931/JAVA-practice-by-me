5b3d776a302e8a79b3d409013ad4852bc53e5e48
Chandani <nittianchandani@gmail.com>
2026-04-08 01:49:20 +0530

    Introduction of Derfault and Parameterized constructor

--- Constructor.java
+++ Constructor.java
@@ -2,10 +2,25 @@ class Human {
     private int age;
     private String name;
 
+    // public Human() { //Default constructor
+    // // System.out.println("In Constructor");
+    // age = 22;
+    // name = "Vikas";
+    // }
+
     public Human() {
-        // System.out.println("In Constructor");
-        age = 23;
-        name = "Vikas";
+        age = 22;
+        name = "Chandani";
+    }
+
+    public Human(String name) {
+        this.age = 21;
+        this.name = name;
+    }
+
+    public Human(int age, String name) { // Parameterized constructor
+        this.age = age;
+        this.name = name;
     }
 
     public int getAge() {
@@ -29,7 +44,10 @@ public class Constructor {
     public static void main(String a[]) {
 
         Human obj = new Human();
+        Human obj1 = new Human(  "Vikas");
+
         System.out.println(obj.getName() + ":" + obj.getAge());
+        System.out.println(obj1.getName() + ":" + obj1.getAge());
 
         // obj.setAge(30);
         // obj.setName("Vikas");
