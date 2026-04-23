1d8e1e5464c2d6a82f9323bf356b5a6f1c414079
Chandani <nittianchandani@gmail.com>
2026-04-24 02:07:42 +0530

    Changing the value of arrys

---
+++ ChangeValue.java
@@ -0,0 +1,14 @@
+
+    import java.util.Arrays;
+    public class ChangeValue {
+        public static void main(String[] args) {
+            // create an array
+
+            int[] arr = { 1, 56, 78, 43, 2, 5, 6 };
+    change(arr);
+    System.out.println(Arrays.toString(arr));
+        }
+        static  void change(int[] nums){
+            nums[0] =78;  //if u  make a change to the object via this refe. variable , sam eobject will  be change 
+        }
+    }
