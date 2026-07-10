e6b2f87fd26f4aba50f977d0366d361a8e46c5ba
Chandani <nittianchandani@gmail.com>
2026-07-10 13:53:47 +0530

    Sum of the given arrays

---
+++ Arrays/Sum.java
@@ -0,0 +1,12 @@
+public class Sum {
+    public static void main(String[] args) {
+        int arr[] = { 2, 4, 7, 89, 34, 76 };
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        System.out.println(sum);
+
+    }
+
+}
