682d8eb356000dfe4db8e2caceb61a50b8c70355
Chandani <nittianchandani@gmail.com>
2026-07-10 13:56:28 +0530

    Reversing the given arrays

---
+++ Arrays/Reverse.java
@@ -0,0 +1,9 @@
+public class Reverse {
+    public static void main(String[] args) {
+        int[] arr = { 2, 4, 6, 7, 8 };
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.println(arr[i]);
+        }
+    }
+
+}
