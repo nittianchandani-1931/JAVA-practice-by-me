6d2343ecbf548be21aeda830a2e0a4f1116604d9
Chandani <nittianchandani@gmail.com>
2026-07-28 00:02:24 +0530

    Added more star patterns

--- pattern.java
+++ pattern.java
@@ -1,8 +1,49 @@
 public class pattern {
     public static void main(String[] args) {
 
+        // for (int i = 0; i < 6; i++) {
+        // for (int j = 0; j <= i; j++) {
+        // System.out.print("*");
+        // } // this
+        // *
+        // **
+        // ***
+        // ****
+        // *****
+        // System.out.println();
+        // }
+
+
+
+        // for (int i = 6; i > 0; i--) {
+        //     for (int j = 0; j <= i; j++) {
+        //         System.out.print("*");// inverted right triangle
+        //     }
+        //     System.out.println();
+        // }
+
+
+//  turned triangle
+        // for (int i = 0; i < 6; i++) {
+        //     for (int j = 0; j < 6 - i; j++) {
+        //         System.out.print(" ");
+        //     }
+        //     for (int j = 0; j <= i; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+
+        // Triangle pattern
         for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6 - i; j++) {
+                System.out.print(" ");
+            }
             for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < i; j++) {
                 System.out.print("*");
             }
             System.out.println();
